8202ec78ecee63ac4c568eea275cbabc541eb110
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-20 15:54:19 -0500

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Dog
+  # attr_accessor :name
+  # attr_accessor :breed
+  # def initialize(name,breed) 
+  #   @name = name
+  #   @breed = breed
+  # end
+  def name=(name)
+    @name = name
+  end
 
+  # getter method
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  # getter method
+  def breed
+    @breed
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+  def name=(name)
+    @name = name
+  end
 
+  # getter method
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  # getter method
+  def job
+    @job
+  end
 end
